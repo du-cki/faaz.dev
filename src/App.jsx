6e4782e9b6d6d28bb72b9b4c7025a7592b1a1b71
du-cki <70286349+du-cki@users.noreply.github.com>
2022-08-01 05:23:58 +0400

    Temp placeholder for now

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,16 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
-import About from './pages/About';
+import Construction from './pages/Construction';
 import NotFound from './pages/404';
-import Projects from './pages/Projects';
-import Project from './pages/Project';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/project/:project" element={<Project />} />
+      <Route path="/about" element={<Construction />} />
+      <Route path="/projects" element={<Construction />} />
+      <Route path="/project/:project" element={<Construction />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
---
+++ src/pages/Construction.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Container from '../components/container';
+
+export default function Construction() {
+  return (
+    <Container>
+      <h1>Oh i&apos;m sorry but i&apos;m still working on this :d</h1>
+    </Container>
+  );
+}
