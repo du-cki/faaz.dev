a1b206059a1df29309ec89cf4c130783c7cb734e
du-cki <qwuakers@gmail.com>
2023-11-23 15:54:23 +0400

    Revised the colours a bit

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,18 +1,18 @@
 // this is just a basic mapping of language: color,
 // its main purpose is cosmetic only but i'd like to get a better solution in the future.
 const languageColourMapping = {
-  rust: '#C36241',
+  rust: '#DEA584',
   python: '#FFD43B',
   html: '#E34C26',
   javascript: '#F0DB4F',
   typescript: '#3178C6',
   vue: '#41B883',
-  css: '#563d7c',
+  css: '#563d7C',
   ruby: '#701516',
-  C: '#555555',
-  emacs_lisp: '#c065db',
+  c: '#555555',
+  emacs_lisp: '#C065DB',
   'c#': '#178600',
-  'c++': '#f34b7d'
+  'c++': '#F34B7D'
 }
 
 export {
