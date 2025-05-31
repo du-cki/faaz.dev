3734e6c1b393c9a83882e23bd7823bdbdca31f3a
du-cki <70286349+du-cki@users.noreply.github.com>
2025-06-01 01:59:04 +0530

    patch: add more language colours

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,7 +1,5 @@
 export const LAST_FM_PLACEHOLDER_IMAGE = 'https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png'
 
-// this is just a basic mapping of language: color,
-// its main purpose is cosmetic only but i'd like to get a better solution in the future.
 export const languageColourMapping = {
   rust: '#DEA584',
   python: '#FFD43B',
@@ -14,9 +12,12 @@ export const languageColourMapping = {
   c: '#555555',
   emacs_lisp: '#C065DB',
   'c#': '#178600',
-  'c++': '#F34B7D'
+  'c++': '#F34B7D',
+  shell: '#89E051',
+  jinja: '#A52A22',
+  go: '#00ADD8',
+  makefile: '#427819',
 }
 
 export const ACTIVITY_COVER_KEY = "discord_activity_covers";
-
 export const ACTIVITY_COVER_EXPIRY = 1000 * 60 * 60 * 24 * 1; // 1 day
